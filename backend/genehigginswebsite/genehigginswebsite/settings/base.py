e90257db4d232784ff7e444a30c506f60e735cbc
Gene Higgins <genotype@gknow.com>
2022-08-24 12:40:24 -0600

    created feature and test to wait for postgres db to spin up

--- backend/genehigginswebsite/genehigginswebsite/settings/base.py
+++ backend/genehigginswebsite/genehigginswebsite/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "home",
     "search",
     "resume",
+    "core",
 
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
@@ -92,8 +93,11 @@ WSGI_APPLICATION = "genehigginswebsite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get('DB_HOST'),
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASS'),
     }
 }
 
